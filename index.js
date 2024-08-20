2fc1dd3649ae3f3a054afc1ab8eb4eb617b8dcef
luantranminh <tranminhluan1206@gmail.com>
2024-08-20 15:18:12 +0700

    feat: add github action

---
+++ index.js
@@ -0,0 +1,18 @@
+const childProcess = require('child_process')
+const os = require('os')
+const process = require('process')
+
+function main() {
+    const binary = "greeting"
+    const mainScript = `${__dirname}/${binary}`
+    const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
+    const status = spawnSyncReturns.status
+    if (typeof status === 'number') {
+        process.exit(status)
+    }
+    process.exit(1)
+}
+
+if (require.main === module) {
+    main()
+}
