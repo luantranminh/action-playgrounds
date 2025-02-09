e8521457504bc24f3b97ff2800d2759086a72b34
luantranminh <tranminhluan1206@gmail.com>
2025-02-09 23:36:36 +0700

    chore: add tf

--- migrations/a.tf
+++ migrations/a.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "example" {
   }
   
 }
+
+resource "aws_instance" "example2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "example-instance"
+  }
+  
+}
